d364fdd6f26086f14868354bd5a82407f995db8a
Max Kutovoy <maxkutovoy@yandex.ru>
2021-10-10 22:25:44 +0600

    Добавил функцию по получению и сохранению картинок

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import requests
 
 import os
 
-directory = "images"
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
+def fetch_images(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
 
-filename = f"{directory}/hubble.jpeg"
-url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    directory = "images"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-response = requests.get(url)
-response.raise_for_status()
+    filename = f"{directory}/{filename}"
 
-with open (filename, "wb") as file:
-    file.write(response.content)
+    with open (filename, "wb") as file:
+        file.write(response.content)
+
+
+if __name__ == "__main__":
+    filename = "hubble.jpeg"
+    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+
+    fetch_images(url, filename)
