622b8403f4703f8c5d3e48c10cd8e635f9e4cc14
Max Kutovoy <maxkutovoy@yandex.ru>
2021-10-11 19:16:32 +0600

    Добавил функцию скачивания фото последнего запуска spacex
    В последнем запуске не всегда есть фото, функция скачивает фото с последнего запуска, в котором фото есть.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 
 import os
+from pprint import pprint
 
 
-def fetch_images(url, filename):
+def save_images(url, filename):
     response = requests.get(url)
     response.raise_for_status()
 
@@ -17,8 +18,24 @@ def fetch_images(url, filename):
         file.write(response.content)
 
 
+def fetch_spacex_last_launch():
+    spacex_url = "https://api.spacexdata.com/v4/launches"
+    spacex_response = requests.get(spacex_url)
+    all_spacex_launches = spacex_response.json()
+    sorted_spacex_launches = list(reversed(all_spacex_launches))
+
+    for flight in sorted_spacex_launches:
+        if flight["links"]["flickr"]["original"]:
+            for image_numder, image_url in enumerate(flight["links"]["flickr"]["original"]):
+                filename = f"spacex{image_numder}.jpg"
+                save_images(image_url, filename)
+            break
+
+
 if __name__ == "__main__":
     filename = "hubble.jpeg"
     url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
-    fetch_images(url, filename)
+    fetch_spacex_last_launch()
+
+    # fetch_images(url, filename)
