cc2bcf3690cd05fd008d8a17662e969ddbff2fd6
Max Kutovoy <maxkutovoy@yandex.ru>
2021-10-14 18:37:33 +0600

    Добавил удаление старых фото после публикации

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import random
+import shutil
 import time
 
 import telegram
@@ -14,13 +15,18 @@ def main():
         bot = telegram.Bot(token=os.getenv("TELEGRAM_TOKEN"))
         space_photos_channel_id = os.getenv("CHANNEL_ID")
 
-        fi.fetch_images()
+        # fi.fetch_images()
         root_img_dir = os.getenv("ROOT_IMG_DIR")
         random_image_dir = random.choice(os.listdir(root_img_dir))
         random_image = random.choice((os.listdir(f"{root_img_dir}/{random_image_dir}")))
         random_image_path = f"{root_img_dir}/{random_image_dir}/{random_image}"
 
         bot.send_photo(chat_id=space_photos_channel_id, photo=open(random_image_path, 'rb'))
+
+        dirs_for_remove = os.listdir(root_img_dir)
+        print(dirs_for_remove)
+        for dir in dirs_for_remove:
+            shutil.rmtree(f"{root_img_dir}/{dir}")
 
         time.sleep(10)
 
