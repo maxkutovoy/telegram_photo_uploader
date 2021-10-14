343d5edbb99d7a71126b93e878e1024854abfce6
Max Kutovoy <maxkutovoy@yandex.ru>
2021-10-14 18:19:53 +0600

    Добавил бота для отправки фото в телеграм канал
    Бот каждый раз обновляет фото перед отправкой

--- bot.py
+++ bot.py
@@ -1,10 +1,29 @@
 import os
-from dotenv import load_dotenv
+import random
+import time
 
 import telegram
+from dotenv import load_dotenv
 
+import fetch_images as fi
 
 load_dotenv()
 
-bot = telegram.Bot(token=os.getenv("TELEGRAM_TOKEN"))
-print(bot.get_me())
+def main():
+    while True:
+        bot = telegram.Bot(token=os.getenv("TELEGRAM_TOKEN"))
+        space_photos_channel_id = os.getenv("CHANNEL_ID")
+
+        fi.fetch_images()
+        root_img_dir = os.getenv("ROOT_IMG_DIR")
+        random_image_dir = random.choice(os.listdir(root_img_dir))
+        random_image = random.choice((os.listdir(f"{root_img_dir}/{random_image_dir}")))
+        random_image_path = f"{root_img_dir}/{random_image_dir}/{random_image}"
+
+        bot.send_photo(chat_id=space_photos_channel_id, photo=open(random_image_path, 'rb'))
+
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
