06e6b4ef838444f491a3800af6e75fd4c12f6a0a
Max Kutovoy <maxkutovoy@yandex.ru>
2021-10-25 23:04:13 +0600

    Исправил опечатку

--- bot.py
+++ bot.py
@@ -12,13 +12,13 @@ def main():
     env = Env()
     env.read_env()
     root_img_dir = env.str("ROOT_IMG_DIR", "images")
-    nasa_toket = env.str("NASA_TOKEN")
+    nasa_token = env.str("NASA_TOKEN")
     telegram_token = env.str("TELEGRAM_TOKEN")
     telegram_channel_id = env.str("CHANNEL_ID")
     bot = telegram.Bot(token=telegram_token)
 
     while True:
-        fetch_nasa(nasa_toket, root_img_dir)
+        fetch_nasa(nasa_token, root_img_dir)
         fetch_spacex_images(root_img_dir)
         random_image_path = images_operations.choose_random_image(root_img_dir)
         with open(random_image_path, 'rb') as file:
